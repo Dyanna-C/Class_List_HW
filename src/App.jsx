f3b27393b3f2d9fdc6151e217e4d838d6bdc48c7
Dyanna <dyanna.person@gmail.com>
2022-11-11 01:15:29 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Kekambas from './components/Kekambas';
+import KekambasClass from './components/KekambasClass';
+
+function App() {
+
+    return (
+        <>
+            <div className="container">
+                <KekambasClass />
+                <Kekambas />
+            </div>
+        </>
+    )
+}
+
+export default App;
